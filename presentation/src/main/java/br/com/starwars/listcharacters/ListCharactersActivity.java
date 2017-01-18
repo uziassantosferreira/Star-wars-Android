51ee60a4df09c5ddc1e435f21428fd5b4c7b6ee4
Uzias Santos Ferreira <Uzias@mbp-de-macbook.home>
2017-01-17 22:48:52 -0300

    change name package listpeople to listcharacters

---
+++ presentation/src/main/java/br/com/starwars/listcharacters/ListCharactersActivity.java
@@ -0,0 +1,10 @@
+package br.com.starwars.listcharacters;
+
+import br.com.starwars.base.BaseActivity;
+
+/**
+ * Created by Uzias on 17/01/17.
+ */
+
+public class ListCharactersActivity extends BaseActivity {
+}
