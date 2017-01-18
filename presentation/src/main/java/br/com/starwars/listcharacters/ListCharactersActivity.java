3d97cfbf46ffdb778ce3774aa9da9777e3839e6b
Uzias Santos Ferreira <Uzias@mbp-de-macbook.home>
2017-01-17 22:54:57 -0300

    add lib butterknife for bindviews and create layout list characters

--- presentation/src/main/java/br/com/starwars/listcharacters/ListCharactersActivity.java
+++ presentation/src/main/java/br/com/starwars/listcharacters/ListCharactersActivity.java
@@ -1,10 +1,29 @@
 package br.com.starwars.listcharacters;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+
+import br.com.starwars.R;
 import br.com.starwars.base.BaseActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Uzias on 17/01/17.
  */
 
 public class ListCharactersActivity extends BaseActivity {
+
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_characters);
+
+        ButterKnife.bind(this);
+
+    }
 }
