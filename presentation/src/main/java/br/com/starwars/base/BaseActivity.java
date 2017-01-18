fed53850e0403f2c206446843fca8533b5a7b217
Uzias Santos Ferreira <Uzias@mbp-de-macbook.home>
2017-01-17 22:48:17 -0300

    create package base for bases views and create package list characters with activity

---
+++ presentation/src/main/java/br/com/starwars/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package br.com.starwars.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Uzias on 17/01/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
